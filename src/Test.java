de4694036fb2765313c02520fee02a3a3fd3cb5e
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-02-01 12:00:50 -0600

    Formatted and orginized code.

--- src/Test.java
+++ src/Test.java
@@ -5,29 +5,28 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println("What is the name of the data file?");
-		Scanner input=new Scanner(System.in);
-		String fileName="src\\"+input.nextLine()+".txt";
+		System.out.println("What is the name of the data file?(exclude .txt)");
+		Scanner input = new Scanner(System.in);
+		String fileName = "src\\" + input.nextLine() + ".txt";
 		File inputFile = new File(fileName);
 		Scanner fileInput = new Scanner(inputFile);
-		String template="";
-			template=template+fileInput.nextLine();
-		//code above store the input file into  template
-		String [] inputStrings=template.split(" ");
-		int sum=0;
+		String template = "";
+		template = template + fileInput.nextLine();
+		// code above store the input file into template
+		String[] inputStrings = template.split(" ");
+		int sum = 0;
 		System.out.println("Odd Numbers: ");
-		for(int i=0; i<inputStrings.length;i++) {
-			if(inputStrings[i].equals("")) {
-				
-			}
-			else {
-				sum+=Integer.parseInt(inputStrings[i]);	
-				if(Integer.parseInt(inputStrings[i]) %2 != 0) {
+		for (int i = 0; i < inputStrings.length; i++) {
+			if (inputStrings[i].equals("")) {
+
+			} else {
+				sum += Integer.parseInt(inputStrings[i]);
+				if (Integer.parseInt(inputStrings[i]) % 2 != 0) {
 					System.out.print(inputStrings[i] + " ");
 				}
 			}
 		}
-		System.out.println("\nThe sum of the integers that are in the file is : "+sum);
+		System.out.println("\nThe sum of the integers that are in the file is : " + sum);
 		//
 	}
 }
