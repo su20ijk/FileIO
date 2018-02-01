4906efde53c6038cdad988c1c308ad3b8b70cb51
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-02-01 11:51:52 -0600

    Deleted file, added odd printout
    Deleted an old and redundent file of mine, I accidentally created two data files. I also added my odd number printout.

--- src/Test.java
+++ src/Test.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
 		System.out.println("What is the name of the data file?");
 		Scanner input=new Scanner(System.in);
-		String fileName=input.nextLine();
+		String fileName="src\\"+input.nextLine()+".txt";
 		File inputFile = new File(fileName);
 		Scanner fileInput = new Scanner(inputFile);
 		String template="";
-		while(fileInput.hasNext()){ 
-			template=template+input.nextLine()+"\n";
-		}
+			template=template+fileInput.nextLine();
 		//code above store the input file into  template
 		String [] inputStrings=template.split(" ");
 		int sum=0;
+		System.out.println("Odd Numbers: ");
 		for(int i=0; i<inputStrings.length;i++) {
-			sum+=Integer.parseInt(inputStrings[i]);
+			if(inputStrings[i].equals("")) {
+				
+			}
+			else {
+				sum+=Integer.parseInt(inputStrings[i]);	
+				if(Integer.parseInt(inputStrings[i]) %2 != 0) {
+					System.out.print(inputStrings[i] + " ");
+				}
+			}
 		}
-		System.out.println("The sum of the integers that are in the file is : "+sum);
-		
-		
+		System.out.println("\nThe sum of the integers that are in the file is : "+sum);
+		//
 	}
 }
